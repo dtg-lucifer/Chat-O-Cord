94f8413212d9c81efb230e7b3d2d599ece57d4f9
srijib <kabird217@gmail.com>
2022-10-09 18:41:05 +0530

    monor bug fixed

--- client/src/pages/SettingsPage/SettingsPage.tsx
+++ client/src/pages/SettingsPage/SettingsPage.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import SettingsBar from '../../components/settingsbar/SettingsBar';
+import SettingsBar from "../../components/_general/sidebars/SettingsBar";
 import ConversationMiniSideBar from "../../components/_general/sidebars/ConversationMiniSideBar";
-import { PageWrapper } from '../../components/_styled/ConversationPage';
-
+import { PageWrapper } from "../../components/_styled/ConversationPage";
 
 const SettingsPage = () => {
   return (
-    <PageWrapper display='flex' fdirection='row'  >
-      <ConversationMiniSideBar />
-      <SettingsBar />
-    </PageWrapper>
+    <>
+      <PageWrapper display="flex" fdirection="row">
+        <ConversationMiniSideBar />
+        <SettingsBar />
+      </PageWrapper>
+    </>
   );
-}
+};
 
 export default SettingsPage
