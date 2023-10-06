5000bf04f69e0dc06c12426f6a2533d5a51bb40c
Piush Bose <piushbasu@gmail.com>
2023-10-07 00:02:44 +0530

    working on authenticated guard on authenticated routes in frontend

---
+++ client-react/src/components/authenticatedGuard/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useLocation, Navigate } from "react-router-dom";
+
+const AuthenticatedGuard: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const { user, isLoading } = useAuth();
+  const location = useLocation();
+
+  if (isLoading) return <div>Loading...</div>
+  if (user) return <>{children}</>
+  return <Navigate to="/auth/login" state={{ from: location }} />
+}
+
+export default AuthenticatedGuard;
